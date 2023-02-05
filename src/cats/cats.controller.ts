e5d840f9c427c4712e4dde96fe40e72b110c17c4
kien6034 <trinhtrungkientb99@gmail.com>
2023-02-05 16:35:08 +0700

    feat: provider and module

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,39 +1,20 @@
 import { Body, Controller, Get, HttpCode, Param, Post, Query, Redirect, Req } from '@nestjs/common';
 import {Request} from 'express';
 import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from 'src/interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
-    @Get()
-    async findAll(@Req() request: Request): Promise<any[]> {
-        return []
-        
-    }
-
-    @Get('ab*cd')
-    findAllWildcard() {
-    return 'This route uses a wildcard';
-    }
-
-    @Get(':id/:id2')
-    @HttpCode(200)
-    findOne(@Param() params) {
-    return `This return a cat with id: ${params.id} ${params.id2}`;
-    }
+    constructor(private catsService: CatsService){}
 
     @Post()
-    create(@Body() createCatDto: CreateCatDto): string {
-        console.log(createCatDto)
-        return 'this add new cats';
-        
+    async create(@Body() createCatDto: CreateCatDto){
+        this.catsService.create(createCatDto)
     }
 
-    @Get('docs')
-    @Redirect('https://docs.nestjs.com', 302)
-    getDocs(@Query('version') version) {
-    if (version && version === '5') {
-        return { url: 'https://docs.nestjs.com/v5/' };
+    @Get()
+    async findAll(): Promise<Cat[]>{
+        return this.catsService.findAll();
     }
-    }
-
 }
